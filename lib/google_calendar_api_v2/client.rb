b81936d6cedf97a3a152ab3f85fa9346e8a65fc1
Parker Young <parker.young@collegeplus.org>
2012-06-04 15:18:50 -0500

    Setting value of connection and calendars

--- lib/google_calendar_api_v2/client.rb
+++ lib/google_calendar_api_v2/client.rb
@@ -1,17 +1,20 @@
 require 'google/api_client'
+require 'socket'
 
 module GoogleCalendarApiV2
   class Client
-    attr_reader :connection, :headers, :calendars, :events
-
+    attr_reader :headers, :calendars, :events
+    attr_accessor :connection
     HEADERS = {'Content-Type' => 'application/json', 'GData-Version' => '2.6'}
 
-    def initialize(client_id, client_secret)
+    def initialize(client_id, client_secret, redirect_uri)
       @client = Google::APIClient.new
-      @client.client_id = client_id 
-      @client.client_secret = client_secret 
-      @client.scope = 'https://www.googleapis.com/auth/calendar'
-      @client.redirect_uri = to('/oauth2callback')
+      @client.authorization.client_id = client_id 
+      @client.authorization.client_secret = client_secret 
+      @client.authorization.scope = 'https://www.googleapis.com/auth/calendar'
+      @client.authorization.redirect_uri = redirect_uri
+      @connection = @client
+      @calendars = Calendar.new(@client)
     end
   end
 end
