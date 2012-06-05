7ddabd0371e490581e620b472807d7fe16339a79
Parker Young <parker.young@collegeplus.org>
2012-06-05 17:16:06 -0500

    Refactoring event class

--- lib/google_calendar_api_v2/event.rb
+++ lib/google_calendar_api_v2/event.rb
@@ -2,65 +2,15 @@ module GoogleCalendarApiV2
   class Event
     include Base
 
-    attr_reader :calendar
+    attr_reader :client, :connection
 
-    def initialize(connection, calendar)
-      @connection = connection
-      @calendar = calendar
+    def initialize(client)
+      @client = client
+      @connection = client.discovered_api('calendar', 'v3')
     end
 
-    def find(event_token, url = nil, redirect_count = 0)
-      url ||= "https://www.google.com/calendar/feeds/#{@calendar.token}/private/full/#{event_token}?alt=jsonc"
-      response = @connection.get url, Client::HEADERS
-
-      raise 'Redirection Loop' if redirect_count > 3
-
-      if success? response
-        item = JSON.parse(response.body)['data']
-        Response::Event.new(item, @connection, @calendar)
-      elsif redirect? response
-        find(event_token, response['location'], redirect_count += 1)
-      end
+    def create(calendar, attrs)
+      @client.execute(api_method: @connection.events.insert, parameters: { 'calendarId' => calendar.id }, body: [JSON.dump(attrs)], headers: {'Content-Type' => 'application/json'})
     end
-
-    def all(url = nil, redirect_count = 0)
-      url ||= "https://www.google.com/calendar/feeds/#{@calendar.token}/private/full?alt=jsonc&futureevents=false"
-      response = @connection.get url, Client::HEADERS
-
-      if success? response
-        # Response::Event.new(response, @connection, @calendar)
-        if items = JSON.parse(response.body)['data']['items']
-          items.map {|item| Response::Event.new(item, @connection, @calendar) }
-        else
-          []
-        end
-      elsif redirect? response
-        all(response['location'], redirect_count += 1)
-      end
-    end
-
-    def create(params = {}, url = nil, redirect_count = 0)
-      url ||= "/calendar/feeds/#{@calendar.token}/private/full?alt=jsonc"
-
-      response = @connection.post url,
-      {
-        :data => {
-          :title => "Undefined event",
-          :hidden => false
-        }.merge(params)
-      }.to_json, Client::HEADERS
-
-      raise 'Redirection Loop' if redirect_count > 3
-
-      if success? response
-        item = JSON.parse(response.body)['data']
-        Response::Event.new(item, @connection, @calendar)
-      elsif redirect? response
-        create(params, response['location'], redirect_count += 1)
-      else
-        false
-      end
-    end
-
   end
 end
