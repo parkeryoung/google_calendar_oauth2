84200eba189ffeedf7648bd18800ad826dc2daf4
Parker Young <parker.young@collegeplus.org>
2012-06-05 17:15:37 -0500

    More work on client model

--- lib/google_calendar_api_v2/client.rb
+++ lib/google_calendar_api_v2/client.rb
@@ -5,7 +5,7 @@ module GoogleCalendarApiV2
   class Client
     attr_reader :headers, :calendars, :events
     attr_accessor :connection
-    HEADERS = {'Content-Type' => 'application/json', 'GData-Version' => '2.6'}
+    HEADERS = {'Content-Type' => 'application/json', 'GData-Version' => '3.0'}
 
     def initialize(client_id, client_secret, redirect_uri)
       @client = Google::APIClient.new
@@ -14,7 +14,22 @@ module GoogleCalendarApiV2
       @client.authorization.scope = 'https://www.googleapis.com/auth/calendar'
       @client.authorization.redirect_uri = redirect_uri
       @connection = @client
-      @calendars = Calendar.new(@client)
+  end
+
+    def callback
+      session[:token] = @client.authorization.fetch_access_token!
+    end
+
+    def redirect_to
+      @client.authorization.authorization_uri.to_s
+    end
+
+    def calendars
+      @calendars = Calendar.new(@connection)
+    end
+
+    def events
+      Event.new(@connection)
     end
   end
 end
