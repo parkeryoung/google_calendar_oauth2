34ed0af26195beb32e85d19c4bf4c7ca2ed127a6
Parker Young <parker.young@collegeplus.org>
2012-06-06 10:32:07 -0500

    Building out event class

--- lib/google_calendar_api_v2/event.rb
+++ lib/google_calendar_api_v2/event.rb
@@ -9,8 +9,19 @@ module GoogleCalendarApiV2
       @connection = client.discovered_api('calendar', 'v3')
     end
 
+    def list(calendar)
+    end
+
+    def find(calendar, query)
+
+    end
+
     def create(calendar, attrs)
       @client.execute(api_method: @connection.events.insert, parameters: { 'calendarId' => calendar.id }, body: [JSON.dump(attrs)], headers: {'Content-Type' => 'application/json'})
+    end
+
+    def update(calendar, event_id, attrs)
+      @client.execute(api_method: @connection.events.insert, parameters: { 'calendarId' => calendar.id, 'eventId' => event_id }, body: [JSON.dump(attrs)], headers: {'Content-Type' => 'application/json'})
     end
   end
 end
