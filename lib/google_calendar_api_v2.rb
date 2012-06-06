c0b50af67d69e3245b3c1b7933de5bdde9178923
Parker Young <parker.young@collegeplus.org>
2012-06-06 13:53:44 -0500

    Getting rid of references to files that don't exist

--- lib/google_calendar_api_v2.rb
+++ lib/google_calendar_api_v2.rb
@@ -1,14 +1,5 @@
 module GoogleCalendarApiV2
   autoload :Client,   'google_calendar_api_v2/client'
-  autoload :Base,     'google_calendar_api_v2/base'
   autoload :Calendar, 'google_calendar_api_v2/calendar'
   autoload :Event,    'google_calendar_api_v2/event'
-
-  autoload :AuthenticationError, 'google_calendar_api_v2/error/authentication'
-
-  module Response
-    autoload :Base,     'google_calendar_api_v2/response/base'
-    autoload :Calendar, 'google_calendar_api_v2/response/calendar'
-    autoload :Event,    'google_calendar_api_v2/response/event'
-  end
 end
