b493f81bd849517a233d19678c0292a37a69ab5f
Parker Young <parker.young@collegeplus.org>
2012-06-08 15:36:32 -0500

    Adding delete event method

--- lib/google_calendar_oauth2/event.rb
+++ lib/google_calendar_oauth2/event.rb
@@ -33,5 +33,9 @@ module GoogleCalendar
       sequence = self.find_by_id(calendar, event_id).sequence + 1
       @client.execute(api_method: @connection.events.update, parameters: { 'calendarId' => calendar.id, 'eventId' => event_id }, body: [JSON.dump(attrs.merge('sequence' => sequence))], headers: {'Content-Type' => 'application/json'})
     end
+  
+    def delete(calendar, event_id)
+      @client.execute(api_method: @connections.events.delete, parameters: { 'calendarId' => calendar.id, 'eventId' => event_id })
+    end
   end
 end
