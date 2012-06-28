829c918fc7def777ed0b56264b799a0db91d4dbb
Parker Young <parker.young@collegeplus.org>
2012-06-28 10:41:56 -0500

    Getting rid of client variable, replacing with connection

--- lib/google_calendar_oauth2/client.rb
+++ lib/google_calendar_oauth2/client.rb
@@ -4,20 +4,18 @@ require 'socket'
 module GoogleCalendar
   class Client
     attr_reader :headers, :calendars, :events
-    attr_accessor :connection
     HEADERS = {'Content-Type' => 'application/json', 'GData-Version' => '3.0'}
 
     def initialize(client_id, client_secret, redirect_uri)
-      @client = Google::APIClient.new
-      @client.authorization.client_id = client_id 
-      @client.authorization.client_secret = client_secret 
-      @client.authorization.scope = 'https://www.googleapis.com/auth/calendar'
-      @client.authorization.redirect_uri = redirect_uri
-      @connection = @client
+      @connection = Google::APIClient.new
+      @connection.authorization.client_id = client_id 
+      @connection.authorization.client_secret = client_secret 
+      @connection.authorization.scope = 'https://www.googleapis.com/auth/calendar'
+      @connection.authorization.redirect_uri = redirect_uri
     end
 
     def redirect_to
-      @client.authorization.authorization_uri.to_s
+      @connection.authorization.authorization_uri.to_s
     end
 
     def calendars
