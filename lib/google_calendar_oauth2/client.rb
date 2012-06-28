514de74dfa0c4f0d623599c5ff7df646ffde73a7
Parker Young <parker.young@collegeplus.org>
2012-06-28 12:56:24 -0500

    Changing the way the connection is passed around

--- lib/google_calendar_oauth2/client.rb
+++ lib/google_calendar_oauth2/client.rb
@@ -2,29 +2,29 @@ require 'google/api_client'
 require 'socket'
 
 module GoogleCalendar
+  def self.connection=(connection)
+    @connection = connection
+  end
+
+  def self.connection
+    @connection
+  end
+
   class Client
     attr_reader :headers, :calendars, :events
     attr_accessor :connection
     HEADERS = {'Content-Type' => 'application/json', 'GData-Version' => '3.0'}
 
     def initialize(client_id, client_secret, redirect_uri)
-      @connection = Google::APIClient.new
-      @connection.authorization.client_id = client_id 
-      @connection.authorization.client_secret = client_secret 
-      @connection.authorization.scope = 'https://www.googleapis.com/auth/calendar'
-      @connection.authorization.redirect_uri = redirect_uri
+      GoogleCalendar.connection = Google::APIClient.new
+      GoogleCalendar.connection.authorization.client_id = client_id 
+      GoogleCalendar.connection.authorization.client_secret = client_secret 
+      GoogleCalendar.connection.authorization.scope = 'https://www.googleapis.com/auth/calendar'
+      GoogleCalendar.connection.authorization.redirect_uri = redirect_uri
     end
 
     def redirect_to
-      @connection.authorization.authorization_uri.to_s
-    end
-
-    def calendars
-      @calendars = Calendar.new(@connection)
-    end
-
-    def events
-      Event.new(@connection)
+      GoogleCalendar.connection.authorization.authorization_uri.to_s
     end
   end
 end
--- lib/google_calendar_oauth2/event.rb
+++ lib/google_calendar_oauth2/event.rb
@@ -1,14 +1,11 @@
 module GoogleCalendar
   class Event
-    attr_reader :client, :connection
-
-    def initialize(client)
-      @client = client
-      @connection = client.discovered_api('calendar', 'v3')
+    def self.connection
+      GoogleCalendar.connection
     end
 
     def list(calendar_id)
-      list = @client.execute(api_method: @connection.events.list, parameters: { 'calendarId' => calendar_id })
+      list = @client.execute(api_method: connection.events.list, parameters: { 'calendarId' => calendar_id })
       list.data.items
     end
 
@@ -22,21 +19,21 @@ module GoogleCalendar
     end
 
     def find_by_id(calendar_id, id)
-      @client.execute(api_method: @connection.events.get, parameters: { 'calendarId' => calendar_id, 'eventId' => id }).data
+      @client.execute(api_method: connection.events.get, parameters: { 'calendarId' => calendar_id, 'eventId' => id }).data
     end
 
     def create(calendar_id, attrs)
-      @client.execute(api_method: @connection.events.insert, parameters: { 'calendarId' => calendar_id }, body: [JSON.dump(attrs)], headers: {'Content-Type' => 'application/json'})
+      @client.execute(api_method: connection.events.insert, parameters: { 'calendarId' => calendar_id }, body: [JSON.dump(attrs)], headers: {'Content-Type' => 'application/json'})
     end
 
     def update(calendar_id, event_id, attrs)
       sequence = self.find_by_id(calendar_id, event_id).sequence
       sequence = sequence.nil? ? 1 : sequence + 1
-      @client.execute(api_method: @connection.events.update, parameters: { 'calendarId' => calendar_id, 'eventId' => event_id }, body: [JSON.dump(attrs.merge('sequence' => sequence))], headers: {'Content-Type' => 'application/json'})
+      @client.execute(api_method: connection.events.update, parameters: { 'calendarId' => calendar_id, 'eventId' => event_id }, body: [JSON.dump(attrs.merge('sequence' => sequence))], headers: {'Content-Type' => 'application/json'})
     end
   
     def delete(calendar_id, event_id)
-      @client.execute(api_method: @connection.events.delete, parameters: { 'calendarId' => calendar_id, 'eventId' => event_id })
+      @client.execute(api_method: connection.events.delete, parameters: { 'calendarId' => calendar_id, 'eventId' => event_id })
     end
   end
 end
