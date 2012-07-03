212c5f3a5287f179e14d88824a9ff03e9025391b
Parker Young <parker.young@collegeplus.org>
2012-07-03 13:41:30 -0500

    Making attributes optional for events.

--- lib/google_calendar_oauth2/event.rb
+++ lib/google_calendar_oauth2/event.rb
@@ -4,7 +4,7 @@ module GoogleCalendar
 
     extend Connection
 
-    def initialize(attrs)
+    def initialize(attrs = {})
       @id = attrs['id']
       @etag = attrs['etag']
       @summary = attrs['summary']
