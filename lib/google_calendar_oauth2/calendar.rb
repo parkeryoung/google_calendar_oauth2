e94b05d4612bbd15ef8b7728bf8cc7ed84773cf5
Peterson Ferreira <petersonferreiras@gmail.com>
2012-08-19 20:32:07 -0300

    revert the name of method of calendar class to create again and add alias insert

--- lib/google_calendar_oauth2/calendar.rb
+++ lib/google_calendar_oauth2/calendar.rb
@@ -16,14 +16,14 @@ module GoogleCalendar
       @cal
     end
 
-    def self.insert(attrs)
+    def self.create(attrs)
       connection.execute(
         api_method: client.calendars.insert,
         body: [JSON.dump(attrs)],
         headers: { "Content-Type" => "application/json" }
       )
     end
-    alias :create :insert
+    alias :insert :create
 
     def self.update(calendar_id, attrs)
       connection.execute(
